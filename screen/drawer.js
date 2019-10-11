92c768a73fd5587ce0b1e12b2db4115fcf4246a4
Anzormumladze <anzor.anzor.mumladze@gmail.com>
2019-10-11 16:04:33 +0400

    drawer swipe fix

--- screen/drawer.js
+++ screen/drawer.js
@@ -1,33 +1,38 @@
 import { createDrawerNavigator } from 'react-navigation-drawer'
-import { createAppContainer } from 'react-navigation';
 import Gela from './gela';
 import Japara from './japara';
 
 
-const MyDrawerNavigator = createDrawerNavigator({
-    Home: {
 
-        screen: Gela,
-        navigationOptions: {
-            title: 'Home',
-            drawerLabel: 'Home'
+
+const MyDrawerNavigator = createDrawerNavigator(
+
+
+
+    {
+
+
+
+
+
+        Home: {
+
+            screen: Gela,
+
+        },
+        Notifications: {
+
+            screen: Japara,
+
+
         }
-    },
-    Notifications: {
-        screen: Japara,
-        navigationOptions: {
-
-            title: 'Settings',
-            drawerLabel: 'Settings',
-
-
-        }
-
-    },
-});
 
 
 
-const MyApp = createAppContainer(MyDrawerNavigator);
+    });
 
-export default MyApp
+
+
+
+
+export default MyDrawerNavigator
